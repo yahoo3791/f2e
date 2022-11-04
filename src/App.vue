bf423514f08fa3d5a4980f8cc71c392d62b8cca5
JIE <youco1327@yahoo.com.tw>
2022-11-05 04:58:21 +0800

    base

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Navbar />
+  <router-view/>
+  <div class="actionBtn">立即報名</div>
+  <div class="sponsorBtn">
+    <img src="@/assets/image/sponsorBlockStudio.png" alt="">
+  </div>
+  <div class="TopBtn"></div>
+  <div class="smallBtn">關卡資訊</div>
+  <div class="mobileBtn"></div>
+  <div class="actionBtnNav">立即報名</div>
+  <Footer />
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/all.scss";
+</style>
+
+<script>
+import Navbar from '@/components/NavbarComponent.vue';
+import Footer from '@/components/FooterComponent.vue';
+
+export default {
+  components: {
+    Navbar, Footer,
+  },
+};
+</script>
